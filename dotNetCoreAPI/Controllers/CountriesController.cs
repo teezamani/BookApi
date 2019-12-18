fc6340c9d9cbe8cc54fb1cd8c6b9c16d0c8dc5c8
teezamani <teezamani@gmail.com>
2019-12-18 13:57:21 +0100

    Created CountriesController for the Api Calls

---
+++ dotNetCoreAPI/Controllers/CountriesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotNetCoreAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotNetCoreAPI.Controllers
+{
+    [Route("api/[countroller]")]
+    [ApiController]
+    public class CountriesController : Controller
+    {
+        private ICountryRepository _countryRepository;
+        public CountriesController(ICountryRepository countryRepository)
+        {
+            _countryRepository = countryRepository;
+        }
+
+        //api/countries
+        [HttpGet]
+        public IActionResult GetCountries()
+        {
+            var countries = _countryRepository.GetCountries().ToList();
+            return Ok(countries);
+        }
+    }
+}
