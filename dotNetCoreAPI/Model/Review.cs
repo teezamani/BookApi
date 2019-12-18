5f91569d52b128a99c41682936a3849a9190b279
teezamani <teezamani@gmail.com>
2019-12-18 08:20:28 +0100

    Created the Models for each class

---
+++ dotNetCoreAPI/Model/Review.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNetCoreAPI.Model
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string HeadLine { get; set; }
+        public string ReviewTest { get; set; }
+        public int Rating { get; set; }
+    }
+}
