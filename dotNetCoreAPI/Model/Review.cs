5d64d482fc101d86c4ef5da00705ead9f9fd5fed
teezamani <teezamani@gmail.com>
2019-12-18 08:49:10 +0100

    Added Dat Annotatins to the Models

--- dotNetCoreAPI/Model/Review.cs
+++ dotNetCoreAPI/Model/Review.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,17 @@ namespace dotNetCoreAPI.Model
 {
     public class Review
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string HeadLine { get; set; }
-        public string ReviewTest { get; set; }
+        [Required]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "Headline must be between 10 and 200 characters")]
+        public string Headline { get; set; }
+        [Required]
+        [StringLength(2000, MinimumLength = 50, ErrorMessage = "ReviewText must be between 50 and 2000 characters")]
+        public string ReviewText { get; set; }
+        [Required]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5 stars")]
         public int Rating { get; set; }
         public virtual Reviewer Reviewer { get; set; }
         public virtual Book Book { get; set; }
