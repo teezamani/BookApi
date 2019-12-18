d60ff17817f0fd9cf3cdcb558903f1dce3a58ffe
teezamani <teezamani@gmail.com>
2019-12-18 12:42:36 +0100

    Created ICountry Repository Interface

---
+++ dotNetCoreAPI/Services/ICountryRepository.cs
@@ -0,0 +1,16 @@
+﻿using dotNetCoreAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNetCoreAPI.Services
+{
+   public interface ICountryRepository
+    {
+        ICollection<Country> GetCountries();
+        Country GetCountry(int countryId);
+        Country GetCountryOfAnAuthor(int authorId);
+        ICollection<Author> GetAuthorsFromACountry(int countryId);
+    }
+}
