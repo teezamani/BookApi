e9180224aaf046895b99f8bff77116fd3aae79ae
teezamani <teezamani@gmail.com>
2019-12-20 09:10:34 +0100

    implementing the ReviewerRepository

---
+++ dotNetCoreAPI/Services/ReviewerRepository.cs
@@ -0,0 +1,45 @@
+﻿using dotNetCoreAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNetCoreAPI.Services
+{
+    public class ReviewerRepository : IReviewerRepository
+    {
+
+        private readonly AppDbContext _reviewerContext;
+
+        public ReviewerRepository(AppDbContext reviewerContext)
+        {
+            _reviewerContext = reviewerContext;
+        }
+
+        public ICollection<Review> GetAllReviewsOfAReviewer(int reviewerId)
+        {
+            return _reviewerContext.Reviews.Where(r => r.Reviewer.Id == reviewerId).ToList();
+        }
+
+        public Reviewer GetReviewer(int reviewerId)
+        {
+            return _reviewerContext.Reviewers.Where(r => r.Id == reviewerId).FirstOrDefault();
+        }
+
+        public Reviewer GetReviewerOfAReview(int reviewId)
+        {
+            var reviewerId = _reviewerContext.Reviews.Where(r => r.Id == reviewId).Select(rr => rr.Reviewer.Id).FirstOrDefault();
+            return _reviewerContext.Reviewers.Where(r => r.Id == reviewerId).FirstOrDefault();
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            return _reviewerContext.Reviewers.OrderBy(r =>r.LastName ).ToList();
+        }
+
+        public bool ReviewerExists(int reviewerId)
+        {
+            return _reviewerContext.Reviewers.Any(r => r.Id == reviewerId);
+        }
+    }
+}
