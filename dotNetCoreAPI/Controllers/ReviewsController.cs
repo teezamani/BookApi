f705d1767ece8cb36cc0931cf11c7a4efd2fd216
teezamani <teezamani@gmail.com>
2019-12-20 15:22:43 +0100

    Finished Api calls for review controller waiting to implent the BookRepository Interface

---
+++ dotNetCoreAPI/Controllers/ReviewsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotNetCoreAPI.Dtos;
+using dotNetCoreAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotNetCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : Controller
+    {
+        private readonly IReviewerRepository _reviewerRepository;
+        private IReviewRepository _reviewRepository;
+        private IBookRepository _bookrepository;
+        public ReviewsController(IReviewerRepository reviewerRepository, IReviewRepository reviewRepository, IBookRepository bookRepository)
+        {
+            _reviewerRepository = reviewerRepository;
+            _reviewRepository = reviewRepository;
+            _bookrepository = bookRepository;
+        }
+
+        //api/reviews
+        [HttpGet]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<ReviewDto>))]
+        public IActionResult GetReviews()
+        {
+            var reviews = _reviewRepository.GetReviews();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var reviewsDto = new List<ReviewDto>();
+            foreach (var review in reviews)
+            {
+                reviewsDto.Add(new ReviewDto
+                {
+                    Id = review.Id,
+                    Headline = review.Headline,
+                    Rating = review.Rating,
+                    ReviewText = review.ReviewText
+                });
+            }
+            return Ok(reviewsDto);
+        }
+
+        //api/reviews/id
+        [HttpGet("{reviewId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<ReviewDto>))]
+        public IActionResult GetReview(int reviewId)
+        {
+            if (!_reviewRepository.ReviewExists(reviewId))
+                return NotFound();
+
+            var review = _reviewRepository.GetReview(reviewId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var reviewDto = new ReviewDto()
+            {
+                Id = review.Id,
+                Headline = review.Headline,
+                Rating = review.Rating,
+                ReviewText = review.ReviewText
+            };
+
+            return Ok(reviewDto);
+        }
+
+        //But we can only test after we Implement IReviewRepository
+        //api/reviews/bookId/reviews
+        [HttpGet("{bookId}/reviews")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<ReviewDto>))]
+        public IActionResult GetAllReviewsOfABook(int bookId)
+        {
+            if (!_bookrepository.BookExists(bookId))
+                return NotFound();
+
+
+            var reviews = _reviewRepository.GetAllReviewsOfABook(bookId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var reviewsDto = new List<ReviewDto>();
+            foreach (var review in reviews)
+            {
+                reviewsDto.Add(new ReviewDto
+                {
+                    Id = review.Id,
+                    Headline = review.Headline,
+                    Rating = review.Rating,
+                    ReviewText = review.ReviewText
+                });
+            }
+            return Ok(reviewsDto);
+        }
+
+        //api/review/reviewId/book
+        [HttpGet("{reviewId}/book")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(BookDto))]
+        public IActionResult GetBookOfAReview(int reviewId)
+        {
+            if (!_reviewRepository.ReviewExists(reviewId))
+                return NotFound();
+
+            var book = _reviewRepository.GetBookOfAReview(reviewId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var bookDto = new BookDto()
+            {
+                Id = book.Id,
+                Title = book.Title,
+                Isbn = book.Isbn,
+                DatePublished = book.DatePublished
+            };
+
+            return Ok(bookDto);
+        }
+    }
+}
