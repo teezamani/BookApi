31f2d403522cff7eaec66e065bb13d1ebf1b11ec
teezamani <teezamani@gmail.com>
2019-12-29 07:41:40 +0100

    Complete CUD Api Calls 4 Reviewer

--- dotNetCoreAPI/Controllers/ReviewersController.cs
+++ dotNetCoreAPI/Controllers/ReviewersController.cs
@@ -1,4 +1,5 @@
 ﻿using dotNetCoreAPI.Dtos;
+using dotNetCoreAPI.Model;
 using dotNetCoreAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -46,7 +47,7 @@ namespace dotNetCoreAPI.Controllers
 
 
         //api/reviewers/id
-        [HttpGet("{reviewerId}")]
+        [HttpGet("{reviewerId}", Name="GetReviewer")]
         [ProducesResponseType(400)]
         [ProducesResponseType(404)]
         [ProducesResponseType(200, Type = typeof(ReviewerDto))]
@@ -126,6 +127,94 @@ namespace dotNetCoreAPI.Controllers
 
             return Ok(reviewerDto);
         }
-        
+
+        //api/reviewers
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(Reviewer))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public IActionResult CreateReviewer([FromBody]Reviewer reviewerToCreate)
+        {
+            if (reviewerToCreate == null)
+                return BadRequest(ModelState);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (!_reviewerRepository.CreateReviewer(reviewerToCreate))
+            {
+                ModelState.AddModelError("", $"Something went wrong saving {reviewerToCreate.FirstName} {reviewerToCreate.LastName}");
+                return StatusCode(500, ModelState);
+            }
+
+            return CreatedAtRoute("GetReviewer", new { reviewerId = reviewerToCreate.Id }, reviewerToCreate);
+        }
+
+
+
+        //api/reviewers/reviewerid
+        [HttpPut("{reviewerId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(204)]//No Content
+
+        public IActionResult UpdateReviewer(int reviewerId, [FromBody]Reviewer reviewerToUpdate)
+        {
+            if (reviewerToUpdate == null)
+                return BadRequest(ModelState);
+
+            if (reviewerId != reviewerToUpdate.Id)
+                return BadRequest(ModelState);
+
+            if (!_reviewerRepository.ReviewerExists(reviewerId))
+                return NotFound();
+
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (!_reviewerRepository.UpdateReviewer(reviewerToUpdate))
+            {
+                ModelState.AddModelError("", $"Something went wrong updating reviewer");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+
+        }
+
+        //api/reviewers/reviewerid
+        [HttpDelete("{reviewerId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(204)]//No Content
+
+        public IActionResult DeleteReviewer(int reviewerId)
+        {
+            if (!_reviewerRepository.ReviewerExists(reviewerId))
+                return NotFound();
+
+            var reviewerToDelete = _reviewerRepository.GetReviewer(reviewerId);
+            var reviewsToDelete = _reviewerRepository.GetAllReviewsOfAReviewer(reviewerId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (!_reviewerRepository.DeleteReviewer(reviewerToDelete))
+            {
+                ModelState.AddModelError("", $"Somethong went wrong deleting {reviewerToDelete.FirstName} {reviewerToDelete.LastName}");
+                return StatusCode(500, $"Somethong went wrong deleting {reviewerToDelete.FirstName} {reviewerToDelete.LastName}");
+            }
+
+            if (!_reviewRepository.DeleteReviews(reviewsToDelete.ToList()))
+            {
+                ModelState.AddModelError("", $"Somethong went wrong deleting reviews by {reviewerToDelete.FirstName} {reviewerToDelete.LastName}");
+                return StatusCode(500, $"Somethong went wrong deleting reviews by {reviewerToDelete.FirstName} {reviewerToDelete.LastName}");
+            }
+
+            return NoContent();
+        }
+
     }
 }
